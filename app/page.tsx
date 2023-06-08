c91a7920c0f13fa2fc442e68a7c21a731a035854
Amerey <info@amerey.eu>
2023-06-08 16:07:33 +0200

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { BadgeDestructive, BadgeOutline, BadgePrimary, BadgeSecondary } from "@/
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <div className="flex max-w-[780px] flex-col items-start gap-2">
         <h2>Custom Table</h2>
         <CustomTable />
 
